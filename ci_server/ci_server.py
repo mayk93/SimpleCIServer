ac6800f5c34ec0a2517a7669597f87dc3fc8ef6e
michael <mihai@mandrescu.co>
2017-11-15 22:28:41 +0000

    Fix.

--- ci_server/ci_server.py
+++ ci_server/ci_server.py
@@ -1,21 +1,54 @@
+# # Server
+# import hug
+# from hug_middleware_cors import CORSMiddleware
+#
+# # Python
+# import os
+# import sys
+# import json
+#
+# # Logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
+#
+#
+# api = hug.API(__name__)
+# api.http.add_middleware(CORSMiddleware(api, allow_origins=["*"]))
+#
+# # Port: 4567
+#
+#
+# @hug.post('/deploy')
+# def deploy(*args, **kwargs):
+#     print("Deploy called")
+#
+#     with open("/tmp/webhook.json", "w+") as dest:
+#         dest.write(json.dumps({
+#             "args": args,
+#             "kwargs": kwargs
+#         }))
+#
+#     return {"ok": True}
+#
+
+print("Starting CI server")
+
 # Server
 import hug
 from hug_middleware_cors import CORSMiddleware
 
 # Python
-import os
-import sys
 import json
 
 # Logging
 import logging
 logging.basicConfig(level=logging.INFO)
 
-
 api = hug.API(__name__)
-api.http.add_middleware(CORSMiddleware(api, allow_origins=["*"]))
+api.http.add_middleware(CORSMiddleware(api))
 
-# Port: 4567
+print("API: %s" % api)
+print("CORSMiddleware: %s" % CORSMiddleware)
 
 
 @hug.post('/deploy')
@@ -29,5 +62,3 @@ def deploy(*args, **kwargs):
         }))
 
     return {"ok": True}
-
-
