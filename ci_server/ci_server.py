fbf72b179116fe770cc55984d2faddd2146c9825
michael <mihai@mandrescu.co>
2017-11-16 22:03:31 +0000

    Setting up webhook response.

--- ci_server/ci_server.py
+++ ci_server/ci_server.py
@@ -19,12 +19,36 @@ def deploy(*args, **kwargs):
     print("New Deploy at %s" % datetime.now())
     try:
         print("Args:")
+    except Exception as e:
+        logging.exception(e)
+    try:
         print(args)
+    except Exception as e:
+        logging.exception(e)
+    try:
         print("Kwargs:")
+    except Exception as e:
+        logging.exception(e)
+    try:
         print(kwargs)
+    except Exception as e:
+        logging.exception(e)
+    try:
         print("\n---\n")
     except Exception as e:
         logging.exception(e)
+
+    try:
+        print(args.__class__.__name__)
+    except Exception as e:
+        logging.exception(e)
+
+    try:
+        print(len(args))
+    except Exception as e:
+        logging.exception(e)
+
+    print("\n=====\n\n")
 
     return {
         "ok": True
