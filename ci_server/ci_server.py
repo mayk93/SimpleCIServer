9e73793949f470a2f7b4595c621bcd68ce69f21d
michael <mihai@mandrescu.co>
2017-11-16 23:28:57 +0000

    Setting up webhook.

--- ci_server/ci_server.py
+++ ci_server/ci_server.py
@@ -3,15 +3,24 @@ import hug
 from hug_middleware_cors import CORSMiddleware
 
 # Python
+import os
 from datetime import datetime
 
 # Logging
 import logging
 logging.basicConfig(level=logging.INFO)
 
+from deploy_handler import DeployHandler
+
 
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api, allow_origins=["https://github.com"]))
+
+PROD = os.environ.get("PROD") == "true"
+if PROD:
+    deploy_handler = DeployHandler()
+else:
+    deploy_handler = DeployHandler(deploy_config_path="deploy_config.json")
 
 
 @hug.post('/deploy')
@@ -48,7 +57,21 @@ def deploy(*args, **kwargs):
     except Exception as e:
         logging.exception(e)
 
+    try:
+        print(kwargs.__class__.__name__)
+    except Exception as e:
+        logging.exception(e)
+
+    try:
+        print(len(kwargs))
+    except Exception as e:
+        logging.exception(e)
+
     print("\n=====\n\n")
+
+    deploy_handler.load_deploy_data(kwargs)
+    deploy_handler.handle_update()
+    deploy_handler.email()
 
     return {
         "ok": True
---
+++ ci_server/deploy_handler.py
@@ -0,0 +1,64 @@
+import os
+import json
+import socket
+
+
+def get_other_data(repo_data):
+    if repo_data is None:
+        return {
+            "updated_at": "unknown",
+            "size": "unknown"
+        }
+    return {
+        "updated_at": repo_data.get("updated_at", "unknown"),
+        "size": repo_data.get("size", "unknown")
+    }
+
+
+class DeployHandler(object):
+    def __init__(self, **kwargs):
+        with open(kwargs.get("deploy_config_path", "/home/deploy/ci_server/deploy_config.json")) as source:
+            self.deploy_config = json.loads(source.read())
+
+        deploy_data = kwargs.get("deploy_data")
+
+        self.repository_name = deploy_data.get("repository", {}).get("name") if deploy_data else None
+        self.branch = deploy_data.get("ref", "").split("/", [None]).reverse()[0] if deploy_data else None
+        self.time = deploy_data.get("head_commit", {}).get("timestamp") if deploy_data else None
+        self.pusher = deploy_data.get("pusher") if deploy_data else None
+
+        self.other_data = None
+
+    def load_deploy_data(self, deploy_data):
+        self.repository_name = deploy_data.get("repository", {}).get("name")
+        self.branch = deploy_data.get("ref", "").split("/", [None]).reverse()[0]
+        self.pusher = deploy_data.get("pusher")
+
+        self.other_data = get_other_data(deploy_data.get("repository"))
+
+    def handle_update(self):
+        print("Now updating for repo with data:")
+        print("Name: %s" % self.repository_name)
+        print("Found in: %s" % os.path.join(self.deploy_config["repo_path"], self.repository_name))
+
+    def email(self):
+        message = '''
+        A new deploy has occurred!
+
+        Repository: %s
+        Updating branch: %s
+        Time of update: %s
+        Pushed by: %s
+
+        Sent from: %s
+        '''.strip() % (
+            self.repository_name,
+            self.branch,
+            self.pusher,
+
+            socket.gethostname()
+        )
+
+        for email in self.deploy_config["email_to"]:
+            print("Sending this email to %s" % email)
+            print("\n-----\n%s\n-----\n" % message)
